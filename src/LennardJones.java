da14780883bacf7fb7312bdccf056035c33f192f
Josiah Claypool <claypooljosiah@gmail.com>
2022-09-27 22:45:21 -0500

    Changed LennardJones parameters
    Added masses, dt instance variables
    
    Removed masses parameter from calcKinetic
    
    Added method updatePositions

--- src/LennardJones.java
+++ src/LennardJones.java
@@ -7,11 +7,14 @@ public class LennardJones {
    int y = 1; // // to make index clearer
    int numOfObjects = 2; // for testing will be 2, make dynamic later
    int totalTime;
-   int boxLength = 5; // for testing will be 5, make dynamic later
+   int boxLength = 5; // for testing will be 5, make dynamic later\
+   double dt = 0.5;
+
    ArrayList<ArrayList<Double>> positions; // for testing will be a based of a boxLength of 10
+   ArrayList<Double> masses;
 
 
-   public LennardJones(int numOfObjects, int totalTime, int boxLength) {
+   public LennardJones(int totalTime) {
       //this.numOfObjects = numOfObjects;
       //this.totalTime = totalTime;
    }
@@ -24,7 +27,7 @@ public class LennardJones {
       return epsilon * 6 / distance * (Math.pow((2 * (sigma / distance)), 12) - Math.pow((sigma / distance), 6));
    }
 
-   public double calcKinetic(ArrayList<Double> masses, ArrayList<ArrayList<Double>> velocities) {
+   public double calcKinetic(ArrayList<ArrayList<Double>> velocities) {
       double kineticEnergy = 0;
       for (int i = 0; i < masses.size(); i++) {
          kineticEnergy += 0.5 * masses.get(i) * (( Math.pow(velocities.get(i).get(x), 2) + Math.pow(velocities.get(i).get(y), 2)));
@@ -43,5 +46,14 @@ public class LennardJones {
          }
       }
       return potentialEnergy;
+   }
+
+   public void updatePositions(ArrayList<ArrayList<Double>> velocities, ArrayList<ArrayList<Double>> forces) {
+      for (int i = 0; i < numOfObjects; i++) {
+         positions.get(i).set(x, positions.get(i).get(x) + velocities.get(i).get(x) * dt + 0.5 *
+                 (forces.get(i).get(x) / masses.get(i)) * Math.pow(dt, 2) );
+         positions.get(i).set(y, positions.get(i).get(y) + velocities.get(i).get(y) * dt + 0.5 *
+                 (forces.get(i).get(y) / masses.get(i)) * Math.pow(dt, 2) );
+      }
    }
 }
