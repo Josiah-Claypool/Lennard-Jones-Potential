0dd6f4f376f9d94f6d9c2732d42df48f32b6379f
Josiah Claypool <claypooljosiah@gmail.com>
2022-09-28 15:49:23 -0500

    Defined DeepCopy class with method deepCopy2D

---
+++ src/DeepCopy.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class DeepCopy {
+
+    /**
+     * Creates a deep copy of a 2D arraylist of doubles to avoid accidentally changing arraylist with shallow copies
+     *
+     * @param theArray The original arraylist that will be deep copied
+     * @return A deep copy of the submitted arraylist
+     */
+
+    public static ArrayList<ArrayList<Double>> deepCopy2D(ArrayList<ArrayList<Double>> theArray) {
+        ArrayList<ArrayList<Double>> fullClone = new ArrayList<>();
+        int width = theArray.get(0).size();
+
+        for (int i = 0; i < width; i++) {
+            ArrayList<Double> rowClone = new ArrayList<>();
+            for (int j = 0; j < width; j++) {
+                rowClone.add(theArray.get(i).get(j));
+            }
+            fullClone.add(rowClone);
+        }
+        return fullClone;
+    }
+}
