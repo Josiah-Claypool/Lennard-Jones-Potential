c961261b951b5d15b8b053d7f293a267cc33a6d9
Josiah Claypool <claypooljosiah@gmail.com>
2022-09-27 21:58:10 -0500

    Fixed a bug for the y instance variable
    Added numOfObjects, totalTime, boxLength instance variables
    
    Added calcPotential method

--- src/LennardJones.java
+++ src/LennardJones.java
@@ -4,11 +4,16 @@ public class LennardJones {
    double epsilon =  4 * 0.0004; // take note of factor of 4, atomic units
    double sigma = 6; // atomic units
    int x = 0; // to make index clearer
-   int y = 0; // // to make index clearer
+   int y = 1; // // to make index clearer
+   int numOfObjects = 2; // for testing will be 2, make dynamic later
+   int totalTime;
+   int boxLength = 5; // for testing will be 5, make dynamic later
+   ArrayList<ArrayList<Double>> positions; // for testing will be a based of a boxLength of 10
 
 
-   public LennardJones() {
-
+   public LennardJones(int numOfObjects, int totalTime, int boxLength) {
+      //this.numOfObjects = numOfObjects;
+      //this.totalTime = totalTime;
    }
 
    public double potential(double distance) {
@@ -25,5 +30,18 @@ public class LennardJones {
          kineticEnergy += 0.5 * masses.get(i) * (( Math.pow(velocities.get(i).get(x), 2) + Math.pow(velocities.get(i).get(y), 2)));
       }
       return kineticEnergy;
+   }
+
+   public double calcPotential() {
+      double potentialEnergy = 0;
+      double r;
+      for (int i = 0; i < numOfObjects; i++) {
+         for (int j = 1; j < numOfObjects; j++) {
+            r =  Math.sqrt( Math.pow( (positions.get(i).get(x) - positions.get(i).get(y)), 2 )  +
+                    Math.pow( (positions.get(j).get(x) - positions.get(j).get(y)), 2 ));
+            potentialEnergy += potential(r);
+         }
+      }
+      return potentialEnergy;
    }
 }
