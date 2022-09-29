3ae47959e68cd065942e0e31856302930c58dfef
Josiah Claypool <claypooljosiah@gmail.com>
2022-09-28 19:18:10 -0500

    Added incomplete step method
    Bug fixes in calcPotential()
    Fixed incorrect indexing on distance calculation
    Fixed inner for loop

--- src/LennardJones.java
+++ src/LennardJones.java
@@ -9,12 +9,13 @@ public class LennardJones {
    int y = 1; // // to make index clearer
    int numOfObjects = 2; // for testing will be 2, make dynamic later
    int totalTime;
-   int boxLength = 5; // for testing will be 5, make dynamic later\
+   double boxLength = 5.0; // for testing will be 5, make dynamic later\
    double dt = 0.5;
    ArrayList<ArrayList<Double>> positions; // for testing will be a based of a boxLength of 10
    ArrayList<Double> masses;
    //ArrayList<ArrayList<Double>> forces;
    ArrayList<ArrayList<Double>> velocities;
+   double time = 0;
 
 
    public LennardJones(int totalTime) {
@@ -53,9 +54,10 @@ public class LennardJones {
       double potentialEnergy = 0;
       double r;
       for (int i = 0; i < numOfObjects; i++) {
-         for (int j = 1; j < numOfObjects; j++) {
-            r =  Math.sqrt( Math.pow( (positions.get(i).get(x) - positions.get(i).get(y)), 2 )  +
-                    Math.pow( (positions.get(j).get(x) - positions.get(j).get(y)), 2 ));
+         for (int j = i + 1; j < numOfObjects; j++) {
+            // replace with distance method later
+            r =  Math.sqrt( Math.pow( (positions.get(i).get(x) - positions.get(j).get(x)), 2 )  +
+                    Math.pow( (positions.get(i).get(y) - positions.get(j).get(y)), 2 ));
             potentialEnergy += potential(r);
          }
       }
@@ -69,7 +71,7 @@ public class LennardJones {
          ArrayList<Double> row = new ArrayList<>(Arrays.asList(0.001 * random.nextDouble(), 0.001 * random.nextDouble()));
          probability = random.nextDouble();
          if (probability < 0.50) {
-            row.set(x, -1.0 * row.get(x));
+            row.set(x, -1.0 * row.get(x)); // could replace with ? maybe
          }
          probability = random.nextDouble();
          if (probability < 0.50) {
@@ -92,6 +94,30 @@ public class LennardJones {
       for (int i = 0; i < numOfObjects; i++) {
          velocities.get(i).set(x, velocities.get(i).get(x) + 0.5 * (forces.get(i).get(x) + nextForces.get(i).get(x)) / masses.get(i) * dt);
          velocities.get(i).set(y, velocities.get(i).get(y) + 0.5 * (forces.get(i).get(y) + nextForces.get(i).get(y)) / masses.get(i) * dt);
+      }
+   }
+
+   public void step() {
+      time += dt;
+      //updatePositions
+      for (int i = 0; i < numOfObjects; i++) {
+         if (positions.get(i).get(x) > boxLength) {
+            positions.get(i).set(x, boxLength);
+            velocities.get(i).set(x, -1 * velocities.get(i).get(x));
+         }
+         if (positions.get(i).get(x) < 0) {
+            positions.get(i).set(x, 0.);
+            velocities.get(i).set(x, -1 * velocities.get(i).get(x));
+         }
+         if (positions.get(i).get(y) > boxLength) {
+            positions.get(i).set(y, boxLength);
+            velocities.get(i).set(y, -1 * velocities.get(i).get(y));
+         }
+         if (positions.get(i).get(y) < 0) {
+            positions.get(i).set(y, 0.);
+            velocities.get(i).set(y, -1 * velocities.get(i).get(y));
+         }
+
       }
    }
 
